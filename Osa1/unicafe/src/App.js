8ddc0da4515b960fb30c16edcd5edc0b66f41bc9
Tomi Isojärvi <isojarvi.tomi@gmail.com>
2022-06-21 23:42:31 +0300

    Osa 1 suoritetty

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -50,7 +50,7 @@ const Statistics = ({ good, neutral, bad }) => {
 
 // App - component
 const App = () => {
-  // States
+  // Hooks
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
