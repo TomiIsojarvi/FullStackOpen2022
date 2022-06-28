84ff31ab1eeb88ea806d33cb2cd5fe3315eb6779
TomiIsojarvi <isojarvi.tomi@gmail.com>
2022-06-28 21:17:09 +0300

    Osa 2b valmis

---
+++ Osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+// App - component
+const App = () => {
+  // HOOKS
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+
+  // EVENT HANDLERS
+  // handleNameChange - Handles the changes in the Name-input field.
+  const handleNameChange = (event) => { setNewName(event.target.value) }
+
+  // handleNumberChange - Handles the changes in the Number-input field.
+  const handleNumberChange = (event) => { setNewNumber(event.target.value) }
+
+  // handleFilterChange - Handles the changes in the Filter-input field.
+  const handleFilterChange = (event) => { setFilter(event.target.value) }
+
+  // addPerson - Adds a new person into the phonebook.
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    // Has the person already been added in the phonebook?
+    const found = persons.find(person => person.name === newName)
+
+    if (found) {
+      // Yes. Person has been already added.
+      alert(`${found.name} has been already added to phonebook.`)
+      return
+    }
+
+    // No. Person has not been added. Let's add it:
+    setPersons(persons.concat(personObject))
+
+    setNewName('')
+    setNewNumber('')
+  }
+
+  // RENDER
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} filterHandler={handleFilterChange} />
+      <h2>Add new person</h2>
+      <PersonForm
+        addPerson={addPerson}
+        name={newName}
+        number={newNumber}
+        nameHandler={handleNameChange}
+        numberHandler={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={persons} filter={filter} />
+    </div>
+  )
+
+}
+
+export default App
